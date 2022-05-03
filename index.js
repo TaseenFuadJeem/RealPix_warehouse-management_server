de945ecd768f80ddd4f36633dcfc4f49979a2593
Md. Ta-Seen Fuad Jeem <mdtaseenfuad@gmail.com>
2022-05-04 02:00:29 +0600

    Added update method

--- index.js
+++ index.js
@@ -66,6 +66,25 @@ async function run() {
 
         })
 
+        app.put('/update-product/:id', async (req, res) => {
+
+            const id = req.params.id;
+
+            const data = req.body;
+
+            const filter = { _id: ObjectId(id) };
+
+            const options = { upsert: true };
+
+            const updatedDoc = {
+                $set: data
+            };
+
+            const result = await camCollection.updateOne(filter, updatedDoc, options);
+
+            res.send(result)
+        })
+
     } finally {
 
     }
